b74703ecae743c0a3934b109c1b37871d2e75f2f
Mikael Kauppinen <1729203+kauppim@users.noreply.github.com>
2024-12-01 21:09:14 +0200

    dec 01

---
+++ 2024/AdventOfCsharp/Program.cs
@@ -0,0 +1,27 @@
+﻿using HistorianHysteria;
+
+internal class Program
+{
+    private static int Main(string[] args)
+    {
+        Console.WriteLine("============AdventOfCsharp============");
+        Console.WriteLine("The following subroutines may be run:");
+        Console.WriteLine(" 1 - Historian Hysteria");
+        Console.WriteLine("=====================================");
+        Console.WriteLine("Please select a subroutine:");
+
+        var input = Console.ReadLine();
+
+        switch (input)
+        {
+            case "1":
+                Console.WriteLine(Hysteria.Calculate());
+            break;
+            default:
+                Console.WriteLine("Good bye!");
+            break;
+        }
+
+        return 0;
+    }
+}
---
+++ 2024/HistorianHysteria/HistorianHysteria.cs
@@ -0,0 +1,38 @@
+﻿namespace HistorianHysteria;
+
+public static class Hysteria
+{
+    public static string Calculate() {
+        uint totalDistance = 0;
+        uint similarityScore = 0;
+        var lines = File.ReadAllLines("/Users/mikael/dev/repos/aoc/2024/HistorianHysteria/testInput");
+        var lefties = new List<int>();
+        var righties = new List<int>();
+        
+        foreach (var line in lines)
+        {
+            var pair = line.Split(' ', 2, StringSplitOptions.RemoveEmptyEntries);
+            lefties.Add(Convert.ToInt32( pair[0] ));
+            righties.Add(Convert.ToInt32( pair[1] ));
+        }
+        lefties.Sort();
+        righties.Sort();
+
+        foreach (var (x, y) in lefties.Zip(righties))
+        {
+            totalDistance += (uint) Math.Abs(x - y);
+        }
+
+        
+
+        foreach (var x in lefties)
+        {
+            var count = righties.Where(el => el == x).Count();
+            similarityScore += (uint)(x * count);
+            Console.WriteLine(similarityScore);
+        }
+        
+        return $"Total distance: {totalDistance}, Similarity score: {similarityScore}";
+    }
+
+}
