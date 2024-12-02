38f2a8e37d26a63838f2dac47b09ae56b4d5e3ac
Mikael Kauppinen <1729203+kauppim@users.noreply.github.com>
2024-12-02 17:07:41 +0200

    dec 02

--- 2024/AdventOfCsharp/Program.cs
+++ 2024/AdventOfCsharp/Program.cs
@@ -1,4 +1,5 @@
 ﻿using HistorianHysteria;
+using RedNosedReports;
 
 internal class Program
 {
@@ -7,7 +8,9 @@ internal class Program
         Console.WriteLine("============AdventOfCsharp============");
         Console.WriteLine("The following subroutines may be run:");
         Console.WriteLine(" 1 - Historian Hysteria");
-        Console.WriteLine("=====================================");
+        Console.WriteLine(" 2a - Red Nosed Reports");
+        Console.WriteLine(" 2b - Red Nosed Reports: Problem dampener");
+        Console.WriteLine("======================================");
         Console.WriteLine("Please select a subroutine:");
 
         var input = Console.ReadLine();
@@ -16,6 +19,12 @@ internal class Program
         {
             case "1":
                 Console.WriteLine(Hysteria.Calculate());
+            break;
+            case "2a":
+                Console.WriteLine(Reports.CalculateSafeReports());
+            break;
+            case "2b":
+                Console.WriteLine(Reports.CalculateSafeReportsDampener());
             break;
             default:
                 Console.WriteLine("Good bye!");
---
+++ 2024/RedNosedReports/Reports.cs
@@ -0,0 +1,85 @@
+﻿namespace RedNosedReports;
+
+public class Reports
+{
+    public static string CalculateSafeReports() {
+        var safeReports = 0;
+        var lines = File.ReadAllLines("/Users/mikael/dev/repos/aoc/2024/RedNosedReports/input");
+        
+        foreach (var line in lines)
+        {
+            var input = line.Split(' ');
+            if (IsReportSafe(input))
+            {
+                safeReports += 1;
+            }
+        }
+
+        return $"Safe reports: {safeReports}";
+    }
+
+    public static string CalculateSafeReportsDampener() {
+        var safeReports = 0;
+        var lines = File.ReadAllLines("/Users/mikael/dev/repos/aoc/2024/RedNosedReports/input");
+        
+        foreach (var line in lines)
+        {
+            var input = line.Split(' ');
+            if (IsReportSafe(input))
+            {
+                safeReports += 1;
+            }
+            else
+            {
+                for (int j = 0; j < input.Length; j++)
+                {
+                    var temp = new List<string>(input.ToList());
+                    temp.RemoveAt(j);
+                    var input2 = temp.ToArray();
+                    if (IsReportSafe(input2))
+                    {
+                        safeReports += 1;
+                        break;
+                    }
+                }
+            }
+        }
+        
+        return $"Safe reports: {safeReports}";
+    }
+
+    private static bool IsReportSafe(string[] input)
+    {
+        var increasing = false;
+        var decreasing = false;
+        var safeMargin = true;
+        for (int i = 0; i < input.Length - 1; i++)
+        {
+            var left = Convert.ToInt32(input[i]);
+            var right = Convert.ToInt32(input[i + 1]);
+            var difference = right - left;
+            if (difference < 0)
+            {
+                decreasing = true;
+            }
+            else
+            {
+                increasing = true;
+            }
+            
+            var absDifference = Math.Abs(difference);
+            if (!(absDifference > 0 && absDifference < 4))
+            {
+                safeMargin = false;
+            }
+        }
+
+        if (safeMargin == true && (increasing != decreasing))
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+}
